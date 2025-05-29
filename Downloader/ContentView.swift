22b689427dacf6680626be8b0a121ce2090c367f
nerf <nerfplx1@gmail.com>
2025-05-30 01:51:47 +0700

    Initial Commit

---
+++ Downloader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Downloader
+//
+//  Created by Alexandr on 30.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Downloader/DownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloaderApp.swift
+//  Downloader
+//
+//  Created by Alexandr on 30.05.25.
+//
+
+import SwiftUI
+
+@main
+struct DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
