8cc55575720ccca2ebc287d0db03b24b81b2de13
nerfplx <75938132+nerfplx@users.noreply.github.com>
2025-05-30 06:27:14 +0700

    Add files via upload

---
+++ backend/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Response, HTTPException
+from pydantic import BaseModel
+import subprocess
+import uuid
+import os
+
+app = FastAPI()
+
+class DownloadRequest(BaseModel):
+    url: str
+    start: float
+    end: float
+
+
+def download_rutube_segment(url, start_time, end_time, output_filename):
+    section = f"*{start_time}-{end_time}"
+
+    cmd = [
+        "yt-dlp",
+        "--download-sections", section,
+        "-o", output_filename,
+        url
+    ]
+
+    print("Запуск yt-dlp...")
+    subprocess.run(cmd, check=True)
+    print("Загрузка завершена.")
+
+
+@app.post("/clip", summary="Скачать отрезок видео")
+def download_clip(req: DownloadRequest):
+
+    output_filename = f"/tmp/{uuid.uuid4()}.mp4"
+
+    try:
+        download_rutube_segment(req.url, req.start, req.end, output_filename)
+
+        with open(output_filename, "rb") as f:
+            video_bytes = f.read()
+
+        return Response(content=video_bytes, media_type="video/mp4")
+
+    except subprocess.CalledProcessError as e:
+        raise HTTPException(status_code=500, detail="Ошибка при скачивании видео")
+
+    finally:
+        if os.path.exists(output_filename):
+            os.remove(output_filename)
